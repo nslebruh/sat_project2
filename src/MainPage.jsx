ddde7fc7da568fde2ed88abed4054b2925362aed
nslebruh <nslebrun1@gmail.com>
2023-03-31 14:57:49 +1100

    changes

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,5 +1,8 @@
 import { threads } from "./util"
-import { useState, useEffect } from "react"
+import { useState, useEffect, Fragment } from "react"
+import {Menu, Transition} from "@headlessui/react"
+import { ChevronDownIcon } from "@heroicons/react/20/solid"
+
 function MainPage() {
   const defaultSort = [
     {
@@ -71,8 +74,8 @@ function MainPage() {
 
 
   return (
-    <div className="bg-neutral-400 h-screen">
-      <div className="bg-neutral-600 h-20">
+    <div className="h-screen bg-neutral-400">
+      <div className="h-20 bg-neutral-600">
         <div>
           <label htmlFor="search">Search</label>
           <input id="search" onInput={(e) => onSearchInput(e)} value={searchValue}/>
@@ -82,15 +85,15 @@ function MainPage() {
         <button className="" onClick={() => changeSortType(2)}>Hex</button>
         <button className="" onClick={() => changeSortType(3)}>Amount</button>
         <button className="" onClick={() => changeSortType(4)}>Do I have it?</button>
-        
+        <MyMenu />
       </div>
       <div>
       Hello world
       </div>
       <ul className="bg-neutral-400">
         {threads_list.map((thread, index) => (
-          <li key={index} className="flex h-16 bg-neutral-300 rounded-lg mx-4 items-center my-2">
-            <div style={{"backgroundColor": `${thread.color}`}} className="h-full w-16"/>
+          <li key={index} className="flex items-center h-16 mx-4 my-2 rounded-lg bg-neutral-300">
+            <div style={{"backgroundColor": `${thread.color}`}} className="w-16 h-full"/>
             <div>
               {thread.floss}
             </div>
@@ -110,6 +113,135 @@ function MainPage() {
         ))}
       </ul>
     </div>
+  )
+}
+
+export function MyMenu() {
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
+  return (
+    <Menu as="div" className="relative inline-block text-left">
+      <div>
+        <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+          Options
+          <ChevronDownIcon className="w-5 h-5 -mr-1 text-gray-400" aria-hidden="true" />
+        </Menu.Button>
+      </div>
+
+      <Transition
+        as={Fragment}
+        enter="transition ease-out duration-100"
+        enterFrom="transform opacity-0 scale-95"
+        enterTo="transform opacity-100 scale-100"
+        leave="transition ease-in duration-75"
+        leaveFrom="transform opacity-100 scale-100"
+        leaveTo="transform opacity-0 scale-95"
+      >
+        <Menu.Items className="absolute right-0 z-10 w-56 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Edit
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Duplicate
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Archive
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Move
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Share
+                </a>
+              )}
+            </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Add to favorites
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+          <div className="py-1">
+            <Menu.Item>
+              {({ active }) => (
+                <a
+                  href="#"
+                  className={classNames(
+                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                    'block px-4 py-2 text-sm'
+                  )}
+                >
+                  Delete
+                </a>
+              )}
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Transition>
+    </Menu>
   )
 }
 
